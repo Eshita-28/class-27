a38b9ba4611aad93c075b4ba8ad0850e8c82427e
Eshita-28 <75899592+Eshita-28@users.noreply.github.com>
2021-01-30 18:48:09 +0530

    Add files via upload

---
+++ Chain.js
@@ -0,0 +1,17 @@
+class Chain{
+    constructor(bodyA,bodyB) {
+        var options = {
+            bodyA:bodyA,
+            bodyB:bodyB,
+            stiffness : 0.04,
+            length:10
+        }
+        this.chain = Constraint.create(options);
+        World.add(world, this.chain);  
+     }
+      display(){
+        strokeWeight(5);
+        line(this.chain.bodyA.position.x,this.chain.bodyA.position.y,this.chain.bodyB.position.x,this.chain.bodyB.position.y);
+        
+      }
+}
